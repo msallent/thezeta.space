50f994dce9e5e54198e574c2e97d766387b468b1
Matias Sallent <matias@reflexai.com>
2025-02-22 00:12:22 -0300

    Opacity changes

--- src/components/packs/Packs.tsx
+++ src/components/packs/Packs.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 
 export function Packs() {
   return (
-    <div className="relative isolate py-24 sm:py-32">
+    <section className="relative isolate py-24 sm:py-32">
       <div
         aria-hidden="true"
         className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
@@ -15,7 +15,7 @@ export function Packs() {
             clipPath:
               'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
-          className="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-70 lg:opacity-40"
         />
       </div>
 
@@ -74,6 +74,6 @@ export function Packs() {
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 }
